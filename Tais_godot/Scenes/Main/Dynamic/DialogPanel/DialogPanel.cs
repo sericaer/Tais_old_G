1abf921ade55a4c70189202c1a31c299a7b0f623
sericaer <sericaer@outlook.com>
2020-08-21 22:08:47 +0800

    Add dialog show

--- Tais_godot/Scenes/Main/Dynamic/DialogPanel/DialogPanel.cs
+++ Tais_godot/Scenes/Main/Dynamic/DialogPanel/DialogPanel.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +9,34 @@ using Modder;
 
 namespace TaisGodot.Scripts
 {
-    class DialogPanel : Panel
-    {
-        public DialogPanel()
-        {
-            Date.Pause();
+	class DialogPanel : Panel
+	{
+		public DialogPanel()
+		{
+			Date.Pause();
 
-            GetNode<Label>("Title").Text = TranslateServerEx.Translate(gEventObj.title.Format, gEventObj.title.Params);
-            GetNode<Label>("Desc").Text = TranslateServerEx.Translate(gEventObj.desc.Format, gEventObj.desc.Params);
+			//GetNode<Label>("Title").Text = TranslateServerEx.Translate(gEventObj.title.Format, gEventObj.title.Params);
+			//GetNode<Label>("Desc").Text = TranslateServerEx.Translate(gEventObj.desc.Format, gEventObj.desc.Params);
 
-            var optionGroup = GetNode<OptionGroup>("options");
-            optionGroup.gmObj = gEventObj.options;
-        }
+			//var optionGroup = GetNode<OptionGroup>("options");
+			//optionGroup.gmObj = gEventObj.options;
+		}
 
-        private void _on_Selected_Signal()
-        {
-            QueueFree();
-        }
+		private void _on_Selected_Signal()
+		{
+			
+		}
 
-        internal GEvent gEventObj;
-    }
+		private void _on_Button_pressed(int extra_arg_0)
+		{
+			// Replace with function body.
+			GD.Print(extra_arg_0);
+			QueueFree();
+
+			Date.UnPause();
+		}
+
+
+		internal GEvent gEventObj;
+	}
 }
--- Tais_godot/Scenes/Main/MainScene.cs
+++ Tais_godot/Scenes/Main/MainScene.cs
@@ -61,6 +61,3 @@ namespace TaisGodot.Scripts
 		//  }
 	}
 }
-
-
-
