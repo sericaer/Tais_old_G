130ac8f809fec78931a885496eb65f60734fc4fd
sericaer <sericaer@outlook.com>
2020-09-03 20:17:25 +0800

    调整MainScene结构

--- Tais_godot/Scenes/Main/Top/Economy.cs
+++ Tais_godot/Scenes/Main/Top/Economy.cs
@@ -1,12 +1,12 @@
-﻿using Godot;
+using Godot;
 using System;
 namespace TaisGodot.Scripts
 {
-    public class Economy : PanelContainer
-    {
-        public override void _Ready()
-        {
-            GetNode<ReactiveLabel>("Button/Economy/Value").Assoc(RunData.Economy.inst.curr);
-        }
-    }
+	public class Economy : Button
+	{
+		public override void _Ready()
+		{
+			GetNode<ReactiveLabel>("HBoxContainer/Value").Assoc(RunData.Economy.inst.curr);
+		}
+	}
 }
