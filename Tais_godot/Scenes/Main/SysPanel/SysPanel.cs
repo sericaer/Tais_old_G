1ed0d23aa3b953f9dd978cd7c2ff6bc9839c2ea0
sericaer <sericaer@outlook.com>
2020-09-07 22:49:51 +0800

    add save panel

--- Tais_godot/Scenes/Main/SysPanel/SysPanel.cs
+++ Tais_godot/Scenes/Main/SysPanel/SysPanel.cs
@@ -21,16 +21,17 @@ namespace TaisGodot.Scripts
 			SpeedContrl.UnPause();
 		}
 
-		private void _on_ButtonSave_button_up(string fileName)
-		{
-			//Root.Load(GlobalPath.save + fileName);
-			GetTree().ChangeScene("res://Scenes/MainScene.tscn");
-		}
-
 		private void _on_Button_Quit_pressed()
 		{
 			GetTree().ChangeScene("res://Scenes/Start/StartScene.tscn");
 		}
 
+		private void _on_Button_Save_pressed()
+		{
+			// Replace with function body.
+		}
 	}
 }
+
+
+
