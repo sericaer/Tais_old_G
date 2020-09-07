5af0478c3e995e66afadcf42b6da2df32e5da9a1
sericaer <sericaer@outlook.com>
2020-09-07 18:05:39 +0800

    增加测试用例

--- NUnitTest/RunData/TestRunData.cs
+++ NUnitTest/RunData/TestRunData.cs
@@ -296,9 +296,70 @@ namespace UnitTest.RunData
         public void TestSerialize()
         {
             Root.Init(def);
+
             var json = Root.Serialize();
 
             Root.Deserialize(json, def);
+
+            Visitor.InitVisitMap(typeof(Root));
+            Visitor.SetVisitData(Root.inst);
+
+            Assert.AreEqual(1, Visitor.Get("date.day"));
+            Assert.AreEqual(1, Visitor.Get("date.month"));
+            Assert.AreEqual(1, Visitor.Get("date.year"));
+
+            Assert.AreEqual(456, Visitor.Get("economy.value"));
+
+            foreach (var income in Root.inst.economy.EnumerateInCome())
+            {
+                switch (income.name)
+                {
+                    case "STATIC_POP_TAX":
+                        Assert.AreEqual(def.economy.pop_tax_percent, income.percent.Value);
+                        Assert.AreEqual(Pop.all.Sum(x => x.expectTax.Value), income.maxValue.Value);
+                        Assert.AreEqual(income.maxValue.Value * income.percent.Value / 100, income.currValue.Value);
+                        break;
+                    default:
+                        Assert.Fail();
+                        break;
+
+                }
+            }
+
+            foreach (var output in Root.inst.economy.EnumerateOutput())
+            {
+                switch (output.name)
+                {
+                    case "STATIC_REPORT_CHAOTING_TAX":
+                        Assert.AreEqual(def.economy.report_tax_percent, output.percent.Value);
+                        Assert.AreEqual(Chaoting.inst.currMonthTax.Value * output.percent.Value / 100, output.currValue.Value);
+                        break;
+                    default:
+                        Assert.Fail();
+                        break;
+
+                }
+            }
+
+            foreach (var departName in def.departs.Keys)
+            {
+                var departDef = def.departs[departName];
+
+                var depart = Depart.GetByColor(departDef.color.r, departDef.color.g, departDef.color.b);
+                Assert.AreEqual(departName, depart.name);
+
+                Assert.AreEqual(departDef.pop_init.Where(x => def.pops[x.name].is_collect_tax).Sum(x => x.num), depart.popNum.Value);
+
+                foreach (var init in departDef.pop_init)
+                {
+                    var pop = depart.pops.Single(x => x.name == init.name);
+                    Assert.AreEqual(init.num, pop.num.Value);
+                }
+            }
+
+            Assert.AreEqual((int)(Depart.all.Sum(x => x.popNum.Value) * def.chaoting.reportPopPercent / 100), Chaoting.inst.reportPopNum.Value);
+            Assert.AreEqual(def.chaoting.taxPercent, Chaoting.inst.taxPercent.Value);
+            Assert.AreEqual(Chaoting.inst.reportPopNum.Value * 0.01 * def.chaoting.taxPercent / 100, Chaoting.inst.currMonthTax.Value);
         }
     }
 }
