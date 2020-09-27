2b975dae57afc23e8a7b7bbba727d11c757f0c06
sericaer <sericaer@outlook.com>
2020-09-27 16:45:43 +0800

    Add TestRandom

---
+++ NUnitTest/Tools/TestRandom.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Parser.Semantic;
+using Parser.Syntax;
+using Tools;
+
+namespace NUnitTest.Tools
+{
+    [TestFixture()]
+    public class TestRandom
+    {
+        public TestRandom()
+        {
+
+        }
+
+        [Test()]
+        public void TestIsOccur()
+        {
+            Assert.True(GRandom.isOccur(100));
+            Assert.False(GRandom.isOccur(0));
+
+            double count = 0;
+            int max = 100000;
+            for(int i=0; i< max; i++)
+            {
+                if(GRandom.isOccur(10))
+                {
+                    count++;
+                }
+            }
+
+            Assert.LessOrEqual(count/max, 0.11);
+            Assert.GreaterOrEqual(count/max, 0.09);
+        }
+
+        [Test()]
+        public void TestCalcGroup()
+        {
+            List<(string, double)> group = new List<(string, double)>()
+            {
+                ("TEST1", 70),
+                ("TEST2", 20),
+                ("TEST3", 10)
+            };
+
+            double Test1Count = 0;
+            double Test2Count = 1;
+            double Test3Count = 1;
+
+            int max = 100000;
+            for (int i = 0; i < max; i++)
+            {
+                var str = GRandom.CalcGroup(group);
+                if (str == "TEST1")
+                {
+                    Test1Count++;
+                }
+                if (str == "TEST2")
+                {
+                    Test2Count++;
+                }
+                if (str == "TEST3")
+                {
+                    Test3Count++;
+                }
+            }
+
+            Assert.LessOrEqual(Test1Count / max, 0.71);
+            Assert.GreaterOrEqual(Test1Count / max, 0.69);
+
+            Assert.LessOrEqual(Test2Count / max, 0.21);
+            Assert.GreaterOrEqual(Test2Count / max, 0.19);
+
+            Assert.LessOrEqual(Test3Count / max, 0.11);
+            Assert.GreaterOrEqual(Test3Count / max, 0.09);
+        }
+    }
+}
